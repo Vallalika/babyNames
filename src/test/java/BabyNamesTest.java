f3f65e172978c392f09970dcf1b2f6b37a4de3b2
Valerie Bayard <valerie.bayard@gmail.com>
2022-10-17 16:20:39 +0100

    added test for getRank method

--- src/test/java/BabyNamesTest.java
+++ src/test/java/BabyNamesTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
@@ -13,20 +14,25 @@ public class BabyNamesTest {
     BabyNames babyNames;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
-    FileResource fr;
 
     @Before
     public void before() {
         System.setOut(new PrintStream(outContent));
         babyNames = new BabyNames();
-        fr = new FileResource("testing/yob2014short.csv");
     }
 
     @Test
     public void totalBirthsTest() {
+        FileResource fr = new FileResource("testing/yob2014short.csv");
         babyNames.totalBirths(fr);
         assertEquals("Total births: 47" + "\n" +
                 "Total names: 10" + "\n" + "Total girls' names: 5" + "\n" + "Total boys' names: 5", outContent.toString().trim());
+    }
+
+    @Test
+    public void test getRanktest() {
+        assertEquals(-1,babyNames.getRank(2012, "Mason", "F");
+        assertEquals(2,babyNames.getRank(2012, "Mason", "M");
     }
 
     @After
